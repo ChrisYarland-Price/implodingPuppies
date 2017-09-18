c84ff310d716c1df0af69aeffd0f38aa767da882
Chris Yarland-Price <chris.yarland@protonmail.com>
2017-09-18 10:32:07 +0100

    the deck loading function is complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,37 @@
 $(function(){
-	console.log("working")
 
 
+	var deck = [];
+	fillDeck(deck, 10, 'test')
+
+		console.log(deck)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	function fillDeck(deck, index, value) {
+		var array = [];
+		for (var i = 0; i < index; i++) {
+			deck.push(value)
+		}
+	}	
 });
